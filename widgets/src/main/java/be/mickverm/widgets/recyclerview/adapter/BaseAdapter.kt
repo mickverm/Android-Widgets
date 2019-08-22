6556033da5f4bdc2efddc903181141407373607f
Michiel Vermeersch <mv@unmatched.eu>
2019-08-22 13:56:16 +0200

    Added inflateLayout() convenience method to BaseAdapter.
    Added hasItems() method to BaseAdapter.

--- widgets/src/main/java/be/mickverm/widgets/recyclerview/adapter/BaseAdapter.kt
+++ widgets/src/main/java/be/mickverm/widgets/recyclerview/adapter/BaseAdapter.kt
@@ -1,5 +1,9 @@
 package be.mickverm.widgets.recyclerview.adapter
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class BaseAdapter<T, VH : RecyclerView.ViewHolder> : RecyclerView.Adapter<VH>() {
@@ -10,6 +14,8 @@ abstract class BaseAdapter<T, VH : RecyclerView.ViewHolder> : RecyclerView.Adapt
 
     fun getItem(position: Int): T = items[position]
 
+    fun hasItems() = items.isNotEmpty()
+
     abstract fun updateItems(items: List<T>)
 
     final override fun onBindViewHolder(holder: VH, position: Int) {
@@ -17,4 +23,7 @@ abstract class BaseAdapter<T, VH : RecyclerView.ViewHolder> : RecyclerView.Adapt
     }
 
     abstract fun onBindViewHolder(holder: VH, position: Int, item: T)
+
+    fun inflateLayout(@LayoutRes layoutRes: Int, parent: ViewGroup): View =
+        LayoutInflater.from(parent.context).inflate(layoutRes, parent, false)
 }
