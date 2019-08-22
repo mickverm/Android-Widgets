b54ac78675103b17739ff899fcf940a9dba0de56
Michiel Vermeersch <mv@unmatched.eu>
2019-08-22 10:03:01 +0200

    Moved compile-, min- and targetSdkVersion declaration to project level build.gradle

--- app/src/main/java/be/mickverm/widgets/sample/MainActivity.kt
+++ app/src/main/java/be/mickverm/widgets/sample/MainActivity.kt
@@ -1,7 +1,7 @@
 package be.mickverm.widgets.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
